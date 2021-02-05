c865b0ba5b24a00e7732fde3212942c58515be16
Katarzyna Kujawa <kkujawa@sumologic.com>
2021-02-05 14:00:40 +0100

    Generate new api - TailingSidecar Used: operator-sdk create api --group=tailing-sidecar --version=v1 --kind=TailingSidecar Resource: yes, Controller: yes

--- operator/main.go
+++ operator/main.go
@@ -26,6 +26,9 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	tailingsidecarv1 "github.com/SumoLogic/tailing-sidecar/operator/api/v1"
+	"github.com/SumoLogic/tailing-sidecar/operator/controllers"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -37,6 +40,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(tailingsidecarv1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -63,6 +67,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.TailingSidecarReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("TailingSidecar"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "TailingSidecar")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
