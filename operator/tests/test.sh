b89149a915f60f1d283bc31b1f40ebc4fdab163e
Katarzyna Kujawa <kkujawa@sumologic.com>
2021-02-15 14:22:21 +0100

    Add new targets for sidecar Makefile Simplify CI jobs for sidecar to use new targets Reword certmanager to cert-manager Make DEPLOYMENT_POD_NAME and DAEMONSET_POD_NAME variables readonly in test script

--- operator/tests/test.sh
+++ operator/tests/test.sh
@@ -13,16 +13,16 @@ set -e
 [[ $(kubectl logs statefulset-with-annotations-0 tailing-sidecar2 -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
 
 # Check Deployment logs
-POD_NAME=$(kubectl get pod -l app=deployment-with-annotations -n tailing-sidecar-system -o jsonpath="{.items[0].metadata.name}")
-[[ $(kubectl logs ${POD_NAME} tailing-sidecar0 -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
-[[ $(kubectl logs ${POD_NAME} tailing-sidecar1 -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
-[[ $(kubectl logs ${POD_NAME} tailing-sidecar2 -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
+readonly DEPLOYMENT_POD_NAME="$(kubectl get pod -l app=deployment-with-annotations -n tailing-sidecar-system -o jsonpath="{.items[0].metadata.name}")"
+[[ $(kubectl logs ${DEPLOYMENT_POD_NAME} tailing-sidecar0 -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
+[[ $(kubectl logs ${DEPLOYMENT_POD_NAME} tailing-sidecar1 -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
+[[ $(kubectl logs ${DEPLOYMENT_POD_NAME} tailing-sidecar2 -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
 
 # Check Daemonset logs
-POD_NAME=$(kubectl get pod -l app=daemonset-with-annotations -n tailing-sidecar-system -o jsonpath="{.items[0].metadata.name}")
-[[ $(kubectl logs ${POD_NAME} tailing-sidecar0 -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
-[[ $(kubectl logs ${POD_NAME} tailing-sidecar1 -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
-[[ $(kubectl logs ${POD_NAME} tailing-sidecar2 -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
+readonly DAEMONSET_POD_NAME="$(kubectl get pod -l app=daemonset-with-annotations -n tailing-sidecar-system -o jsonpath="{.items[0].metadata.name}")"
+[[ $(kubectl logs ${DAEMONSET_POD_NAME} tailing-sidecar0 -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
+[[ $(kubectl logs ${DAEMONSET_POD_NAME} tailing-sidecar1 -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
+[[ $(kubectl logs ${DAEMONSET_POD_NAME} tailing-sidecar2 -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
 
 echo "ok"
 exit 0
---
+++ sidecar/tests/test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+[[ $(kubectl logs --tail 5 example-with-tailling-sidecars sidecar1 | grep example1 | wc -l) -ne 5 ]] && exit 1
+[[ $(kubectl logs --tail 5 example-with-tailling-sidecars sidecar2 | grep example2 | wc -l) -ne 5 ]] && exit 1
+
+echo "ok"
+exit 0
