d6db007415fb1836cbca8978eeb3a8c3efae7440
Katarzyna Kujawa <kkujawa@sumologic.com>
2021-03-02 17:35:53 +0100

    Make sidecar container name configurable Add environmental variable TAILING_SIDECAR=true to distinguish sidecar containers Change isSidecarEnvAvailable function adding env name as argument

--- operator/tests/test.sh
+++ operator/tests/test.sh
@@ -5,11 +5,11 @@ set -e
 # Check Pod logs
 [[ $(kubectl logs pod-with-annotations tailing-sidecar0 -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
 [[ $(kubectl logs pod-with-annotations tailing-sidecar1 -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
-[[ $(kubectl logs pod-with-annotations tailing-sidecar2 -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
+[[ $(kubectl logs pod-with-annotations named-container -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
 
 # Check StatefulSet logs
 [[ $(kubectl logs statefulset-with-annotations-0 tailing-sidecar0 -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
-[[ $(kubectl logs statefulset-with-annotations-0 tailing-sidecar1 -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
+[[ $(kubectl logs statefulset-with-annotations-0 my-named-sidecar -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
 [[ $(kubectl logs statefulset-with-annotations-0 tailing-sidecar2 -n tailing-sidecar-system --tail 5 | grep example | wc -l) -ne 5 ]] && exit 1
 
 # Check Deployment logs
